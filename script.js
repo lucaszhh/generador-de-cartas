1b857fd80a5228f1de64f4251d291b1a13c0ebb3
Lucas Zarandón <86167490+lucaszhh@users.noreply.github.com>
2023-08-10 01:46:09 -0300

    clase para crear cartas de manera dinamicaaaa

---
+++ script.js
@@ -0,0 +1,50 @@
+/* Este es el listado de la información de las cartas  */
+const listadoCartas = [
+    {
+        id: 1,
+        nombre: "",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+    {
+        id: 2,
+        nombre: "",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+    {
+        id: 3,
+        nombre: "",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+    {
+        id: 4,
+        nombre: "",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+    {
+        id: 5,
+        nombre: "",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+]
+
+
+/* Selección de los NODOS HTML (etiquetas/elementos html) */
+const contenedorCartas = document.querySelector("")
+
+/* Bucle for */
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+
+/* Función que recorre el listado de las cartas */
+listadoCartas.forEach(
+    () => contenedorCartas.innerHTML += ``
+)
+
+
+
